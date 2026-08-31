5ab4a4e385fd589513bfea3b8f9e3d8d83535e2f
Matheus <matheusraulinoo@gmail.com>
2026-08-31 11:03:56 -0300

    docs: add forum

--- project/settings.py
+++ project/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'forum',
+    'hello',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
